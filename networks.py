692f7d2422cc36cdb374e975238ef66fa6857fb5
agango <ag3661@columbia.edu>
2018-04-18 00:13:50 +0000

    adding residual block

--- networks.py
+++ networks.py
@@ -186,7 +186,9 @@ def G_paper(
                     x = PN(act(apply_bias(x)))
                 with tf.variable_scope('Conv'):
                     x = PN(act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, use_wscale=use_wscale))))
+		    
             else: # 8x8 and up
+		x_input = upscale2d(x)
                 if fused_scale:
                     with tf.variable_scope('Conv0_up'):
                         x = PN(act(apply_bias(upscale2d_conv2d(x, fmaps=nf(res-1), kernel=3, use_wscale=use_wscale))))
@@ -196,6 +198,7 @@ def G_paper(
                         x = PN(act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, use_wscale=use_wscale))))
                 with tf.variable_scope('Conv1'):
                     x = PN(act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, use_wscale=use_wscale))))
+		    x = tf.add(x, x_input)
             return x
     def torgb(x, res): # res = 2..resolution_log2
         lod = resolution_log2 - res
