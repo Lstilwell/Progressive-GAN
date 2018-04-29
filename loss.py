7236515c9a285b1c57aada9d1aec579b27b430bf
agango <ag3661@columbia.edu>
2018-04-29 19:41:07 +0000

    porting over least squares

--- loss.py
+++ loss.py
@@ -97,5 +97,26 @@ def D_wgangp_acgan(G, D, opt, training_set, minibatch_size, reals, labels,
             label_penalty_fakes = tfutil.autosummary('Loss/label_penalty_fakes', label_penalty_fakes)
         loss += (label_penalty_reals + label_penalty_fakes) * cond_weight
     return loss
-
 #----------------------------------------------------------------------------
+def G_lsgan(G, D, opt, training_set ,minibatch_size, cond_weight=1.0):
+	latents = tf.random_normal([minibatch_size] + G.input_shapes[0][1:])
+	labels = training_set.get_random_labels_tf(minibatch_size)
+	fake_images_out = G.get_output_for(latents,labels,is_training=True)
+	fake_scores_out, fake_labels_out = fp32(D.get_output_for(fake_images_out,is_training=True))
+	
+	loss = tf.reduce_sum(tf.square(fake_scores_out-1))/2
+	return loss
+
+def D_lsgan(G, D, opt, training_set, minibatch_size, reals, labels):
+	latents = tf.random_normal([minibatch_size] + G.input_shapes[0][1:])
+	fake_images_out = G.get_output_for(latents,labels,is_training=True)
+	real_scores_out, real_labels_out = fp32(D.get_output_for(reals,is_training=True))
+	fake_scores_out, fake_labels_out = fp32(D.get_output_for(fake_images_out, is_training=True))
+	real_scores_out = tfutil.autosummary('Loss/real_scores',real_scores_out)
+	fake_scores_out = tfutil.autosummary('Loss/fake_scores',fake_scores_out)
+
+	loss = tf.reduce_sum(tf.square(real_scores_out-1) + tf.square(fake_scores_out))/2
+	return loss
+
+
+
